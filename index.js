0cff05799019372825a9a12eadc35c578beba9c2
Evans <evans.kibet@student.moringaschool.com>
2022-08-10 10:14:02 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const element = document.querySelector("main#main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.textContent = "h1#victory";
+
+newHeader.id = "victory";
+
+newHeader.textContent = "h1#victory YOUR-NAME is the champion";
